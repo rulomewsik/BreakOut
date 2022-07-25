deec8a83cacf1463acab48ea9bb79e29bfcd4ad7
Raul Vega <thedevi4nt@outlook.es>
2022-07-25 17:06:39 -0500

    Se agregaron los cambios para tarea módulo 12

--- BreakOut/Assets/Scripts/Ball.cs
+++ BreakOut/Assets/Scripts/Ball.cs
@@ -1,18 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public bool isGameStarted;
+    [SerializeField] public float ballSpeed = 20.0F;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        var initialPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+        initialPosition.y += 2;
+        transform.position = initialPosition;
+        transform.SetParent(GameObject.FindGameObjectWithTag("Player").transform);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space) || Input.GetButton("Submit"))
+        {
+            if (!isGameStarted)
+            {
+                isGameStarted = true;
+                transform.SetParent(null);
+                GetComponent<Rigidbody>().velocity = ballSpeed * Vector3.up;
+            }
+        }
     }
 }
